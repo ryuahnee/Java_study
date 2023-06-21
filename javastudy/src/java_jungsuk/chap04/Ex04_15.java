a685d1deacc17843f0f3698274dc9a8e09b48197
ryuahneee <ryuahneee@gmail.com>
2023-06-21 18:05:27 +0900

    ex23-06-21

--- javastudy/src/java_jungsuk/chap04/Ex04_15.java
+++ javastudy/src/java_jungsuk/chap04/Ex04_15.java
@@ -8,9 +8,10 @@ public class Ex04_15 {
 		int tmp = number;
 		int result =0; // number 변수 를 거꾸로 변환해서 담을 변수
 		while(tmp !=0) {
-		/*
-		(1) . 알맞은 코드를 넣어 완성하시오
-		*/
+			for(int i =1; i < 100000; i=i*10) {
+				result = result+(tmp%10)*i; 
+				tmp = tmp/10;
+			}
 		}
 		if(number == result) {
 			System.out.println( number + " 는 회문수 입니다 .");
