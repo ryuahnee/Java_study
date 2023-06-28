aa4d046b4a90676f506bcd8080e24d3240aa4ee6
ryuahneee <ryuahneee@gmail.com>
2023-06-28 09:36:35 +0900

    commit2

---
+++ javastudy/src/java_jungsuk/chap05/Ex05_05.java
@@ -0,0 +1,29 @@
+package java_jungsuk.chap05;
+
+
+public class Ex05_05 {
+
+	public static void main(String[] args) {
+		
+		int[] ballArr = {1,2,3,4,5,6,7,8,9};
+		int[] ball3 = new int[3];
+		
+		// 배열 ballArr의 임의의 요소를 골라서 위치를 바꾼다
+		for(int i=0; i< ballArr.length;i++) {
+			int j = (int)(Math.random() * ballArr.length);
+			int tmp = 0;
+			if(j != tmp) {
+				tmp = j;
+			for(int k = 0; k<ball3.length; k++) {
+					ball3[k] = tmp;
+					//d
+				}
+			}
+		}
+		for(int i=0;i<ball3.length;i++) {
+			System.out.print(ball3[i]);
+		}
+		
+	}//main
+
+}
